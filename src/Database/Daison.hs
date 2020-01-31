ac1be5e7f04ab89373624844a0e7d0d3e2232297
krangelov <kr.angelov@gmail.com>
2020-01-31 08:44:50 +0100

    export the QueryMonad class

--- src/Database/Daison.hs
+++ src/Database/Daison.hs
@@ -18,7 +18,7 @@ module Database.Daison
             , dropDataSet, tryDropDataSet
             , insertData, deleteData
 
-            , Query
+            , Query, QueryMonad
             , select, query, anyOf
 
             , Aggregator
